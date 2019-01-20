9cea3941af344e8b6199e826cd81d7453810e000
hadaytullah@gmail.com <hadaytullah@gmail.com>
2019-01-20 16:00:00 +0000

    sensor get and post initialiazed

--- app.py
+++ app.py
@@ -3,17 +3,14 @@ from flask import Flask
 import jwt
 from sensor import Sensor
 
-secret_key = 'Ki39krMoE534'
 app = Flask(__name__)
 
 @app.route('/')
 def test():
     return 'API is alive.'
 
-#@app.route('/sensor/<string:sensorid>',methods = ['GET'])
-#def get_sensor(sensorid):
-#    return 'You are sensor ' + sensorid + '.'
-
-app.add_url_rule('/sensor/<string:sensorid>', view_func=Sensor.as_view('sensor'))
+sensor_view_func = Sensor.as_view('sensor')
+app.add_url_rule('/sensor', view_func=sensor_view_func)
+app.add_url_rule('/sensor/<sensorid>', view_func=sensor_view_func)
 
 app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
--- sensor.py
+++ sensor.py
@@ -1,15 +1,27 @@
 #author: hadaytullah kundi, hadaytullah@gmail.com
 from flask.views import MethodView
-
+from flask import jsonify
+from flask import request
 
 class Sensor(MethodView):
     """
         Sensor api entry points handler, GET,POST,PUT,DELETE
     """
     def get(self, sensorid):
-        return 'You are sensor ' + sensorid + '.'
+        return jsonify(
+                 code='200',
+                 message='Sensor data ' + sensorid + ' returned.'
+                )
 
-    #def post(self):
+    def post(self):
+        sensorid='002'
+        print(request.data)
+        return jsonify(
+                 code='200',
+                 message='Sensor registered ' + sensorid + '.',
+                 payload=request.data
+                )   
+    
     #def put(self):
     #def delete(self):
         
