06f72d1a593a4ace24399117b6a1e87653161d91
hadaytullah@gmail.com <hadaytullah@gmail.com>
2019-01-23 16:33:26 +0000

    data POST endpoint implemented

---
+++ api/data.py
@@ -0,0 +1,79 @@
+#author: hadaytullah kundi, hadaytullah@gmail.com
+import traceback, json
+from flask.views import MethodView
+from flask import jsonify
+from flask import request
+from flask import abort, make_response
+from bson.objectid import ObjectId
+from shared import db, log
+
+class Data(MethodView):
+    """
+        Data api entry points handler, GET,POST,PUT,DELETE
+        
+        location is { lat, lng}
+    """
+
+    def post(self):
+        try:
+            # encode/decode
+            print(request.data.decode('utf-8'))
+            data_dict = json.loads(request.data.decode('utf-8'))
+            # Store data
+            document = db.data.insert_one({
+                'sensor_id':data_dict['sensor_id'],
+                'speed':data_dict['speed'],
+                'speed_unit': data_dict['speed_unit'],
+                'location':data_dict['location'],
+                'location_accuracy':data_dict['location_accuracy'],
+            })
+            print(document.inserted_id)
+            if document.inserted_id: #if inserted successfully
+                return jsonify(
+                    string_code='DATA_INSERTED',
+                    message='Sensor data stored in the database.',
+                    data= {
+                        'id':document.inserted_id.__str__()
+                    }
+                )
+            else:
+                # do not expose what went wrong to the callee, use a logger to log it for internal use
+                log.info('Unable to insert data into db')
+                log.info(request.data)
+                
+                return abort(make_response(jsonify(
+                    string_code='SERVER_ERROR',
+                    message='Something went wrong.',
+                    data=None
+                ),500))
+            
+        except Exception as e: #NOTE: can't catch KeyboardInterrupt and SystemExit
+            # do not expose what went wrong to the callee, use a logger to log it for internal use
+            log.error(traceback.format_exc())
+            
+            return abort(make_response(jsonify(
+                string_code='SERVER_ERROR',
+                message='Something went wrong.',
+                data=None
+            ),500))
+            
+    def get(self, sensor_id):
+       return abort(make_response(jsonify(
+            string_code='NOT_IMPLEMENTED',
+            message='This api endpoint has not been implemented.',
+            data=None
+        ),501))
+        
+    def put(self, sensor_id):
+        return abort(make_response(jsonify(
+            string_code='NOT_IMPLEMENTED',
+            message='This api endpoint has not been implemented.',
+            data=None
+        ),501))
+    
+    def delete(self, sensor_id):
+        return abort(make_response(jsonify(
+            string_code='NOT_IMPLEMENTED',
+            message='This api endpoint has not been implemented.',
+            data=None
+        ),501))
--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 import jwt
 from api.sensor import Sensor
-
+from api.data import Data
 app = Flask(__name__)
 
 #routes        
@@ -14,6 +14,12 @@ sensor_view_func = Sensor().as_view('sensor')
 app.add_url_rule('/sensors/', defaults={'sensor_id':None}, view_func=sensor_view_func, methods=['GET',])
 app.add_url_rule('/sensors/', view_func=sensor_view_func, methods=['POST',])
 app.add_url_rule('/sensors/<sensor_id>/', view_func=sensor_view_func, methods=['GET', 'PUT', 'DELETE'])
+#app.add_url_rule('/sensor/<sensor_id>/data', view_func=data_view_func, methods=['POST',])
+
+data_view_func = Data().as_view('data')
+#app.add_url_rule('/data/', defaults={'sensor_id':None}, view_func=data_view_func, methods=['GET',])
+app.add_url_rule('/data/', view_func=data_view_func, methods=['POST',])
+#app.add_url_rule('/data/<sensor_id>/', view_func=data_view_func, methods=['GET', 'PUT', 'DELETE'])
 
 #start the api server
 app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
