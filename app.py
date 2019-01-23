1dbc3b10c61979f6577e69545379f7fa04ae41d6
hadaytullah@gmail.com <hadaytullah@gmail.com>
2019-01-23 14:09:28 +0000

    sensor api interface updated to /sensors/ from /sensor/

--- app.py
+++ app.py
@@ -11,8 +11,9 @@ def test():
     return 'API is alive.'
 
 sensor_view_func = Sensor().as_view('sensor')
-app.add_url_rule('/sensor', view_func=sensor_view_func)
-app.add_url_rule('/sensor/<sensor_id>', view_func=sensor_view_func)
+app.add_url_rule('/sensors/', defaults={'sensor_id':None}, view_func=sensor_view_func, methods=['GET',])
+app.add_url_rule('/sensors/', view_func=sensor_view_func, methods=['POST',])
+app.add_url_rule('/sensors/<sensor_id>/', view_func=sensor_view_func, methods=['GET', 'PUT', 'DELETE'])
 
 #start the api server
 app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
